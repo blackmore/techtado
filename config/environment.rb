5baa20225f0c1cb69e2d82e72fa3eb926c43de1e
Nigel Blackmore <nigel@nigel-blackmores-macbook.local>
2009-03-02 15:15:04 +0100

    removed factory_girl

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+  #config.gem "thoughtbot-", :lib => "factory_girl", :source => "http://gems.github.com"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
